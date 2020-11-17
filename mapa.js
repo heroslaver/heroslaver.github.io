3e75de5c9e6cdc5c31e1988eadeb4c9d489eecc7
SEBASTIAN PEÑARANDA <sebastian.penaranda@correounivalle.edu.co>
2020-11-17 23:40:25 +0000

    11/17/2020

--- mapa.js
+++ mapa.js
@@ -9,13 +9,41 @@
   
 
   //Hace funcionar el reconocimiento de cada celda en el mapa, si se quita deja de funcionar
+
   function recursivelist(l, f, index = 0) {
     if (!isEmpty(l)) {
       f(first(l), index);
       recursivelist(rest(l), f, index + 1);
     }
   }
+  
+  /*
+  Contrato: posicion list, number --> number
+  Funcionalidad: Permite saber la posicion en el eje X o Y segun el elegido.
+  posicion(){Cuerpo de la funcion}
+  Ejemplos:
+  posicion([1,0,0,0,2,0,3], 4)= 2
+  posicion([1,0,0,0,2,0,3],0)= 1
+  */
 
+  function posicion(lista,y)
+  { 
+    if(y==0)
+    return first(lista);
+    else
+    return posicion(rest(lista),y-1);
+    
+  }
+  
+  /*
+    Contrato: calcularPosicionTomb number, number --> structure
+    Funcionalidad: Calcula la posicion en cuadricula cuando esta es multiplo de 40.
+    calcularPosicionTomb(){Cuerpo de la funcion}
+    Ejemplos:
+    calcularPosicionTomb([400,520])= [x:10,y:13]
+    calcularPosicionTomb([0,40,])= [x:0,y:1]
+  */
+  
   function calcularPosicionTomb(x,y)
   {
     
@@ -31,33 +59,97 @@
     posicionActualTomb={x:auxiliarx,y:auxiliary}
     return posicionActualTomb;
   }
+  
   /*
-  function colision(world)
+    Contrato: colisionTomb number, number --> number
+    Funcionalidad: Dice el dato en la posicion de la matriz dada, en este caso el mapa en cuestion.
+    colisionTomb(){Cuerpo de la funcion}
+    Ejemplos:
+    colisionTomb([10,13])= 2
+    colisionTomb([0,2])= 1
+  */
+
+  function colisionTomb(x,y)
   {
-    if(world.tomb.x + world.tomb.width >= WIDTH)
+    return posicion(posicion(mapa,y),x);
+  }
+
+  /*
+    Contrato: recorrerBloques list, number, number --> number
+    Funcionalidad: Compara la lista con el dato de la matriz y retorna verdadero si el dato aparece en la lista, y falso si el dato no aparece en la lista.
+    recorrerBloques(){Cuerpo de la funcion}
+    Ejemplos:
+    recorrerBloques([listaBloques,0,2])= true
+    recorrerBloques([listaBloques,10,13])= false
+  */
+
+  function recorrerBloques(lista,x,y)
+  {
+    if(isEmpty(lista))
     {
-      make(world, {tomb:{dirx:0}});
+      return false;
     }
-    if(world.tomb.x <= 0)
+    else if (colisionTomb(x,y) == first(lista))
     {
-      make(world, {tomb:{dirx:0}});
+      return true;
     }
-    if(world.tomb.y+ world.tomb.height >= HEIGHT)
+    else
     {
-      make(world, {tomb:{diry:0}});
-    }
-      if(world.tomb.y <= 0)
-    {
-      make(world, {tomb:{diry:0}});
+      return recorrerBloques(rest(lista),x,y);
     }
   }
+  
+  /*
+    Contrato: nombre, number, number --> number
+    Funcionalidad: .
+    nombre(){Cuerpo de la funcion}
+    Ejemplos:
   */
+
+  function comeMoneda(p, lista){
+      if(isEmpty(lista)){
+      return [];
+      }
+      if(p>=0 && p<=(lista).length){ //entre cero y la cantidad de datos de la lista, porque la va a recorrer
+      if (p==0){
+      return append(cons(0,[]),rest((lista)));
+      }else{
+        return append(cons(first(lista),[]),comeMoneda(p-1, (rest(lista))));
+      }
+     }else{
+      return lista; 
+     }
+  }
+  
+  /*
+    Contrato: nombre, number, number --> number
+    Funcionalidad: .
+    nombre(){Cuerpo de la funcion}
+    Ejemplos:
+  */
+
+  // reemplaza la nueva lista sin la moneda
+  function replaceX(x,lista, elem){
+    if(x>=0 && x<lista.length){
+      if(isEmpty(lista)){
+        return [];
+      }else if(x==0){
+        return cons(elem,rest(lista));
+      }else if(x==1){
+        return cons(first(lista),cons(elem,rest(rest(lista)))); 
+      }else {
+      return cons(first(lista),replaceX(x-1,rest(lista),elem)); 
+      }
+    }else{
+      return lista;
+    }
+  }  
+
+  //tamaño estandar de todo dentro del mapa
   const SIZE = 40;
-  //tamaño de ancho y largo
+  //tamaño ideal de los dibujos (especificamente seria el ancho y largo de cada bloque)
   const WIDTH = 24*SIZE;
   const HEIGHT = 15*SIZE;
-  //tamaño ideal de los dibujos (especificamente seria el ancho y largo de cada bloque)
-  
 
   //Variables iniciadas en nullptr
   let tomb = null;
@@ -66,9 +158,13 @@
   let MURO3 = null;
   let MURO4 = null;
   let MONEDA =null;
-
+  //Variables utilizadas dentro de funciones.
+  let quietoX= false;
+  let quietoY= false;
+  let listaBloques=[1,3,4,5];
+  
   //Matriz del mapa
-  const mapa = 
+  var mapa = 
   [
 				[5, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 5, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1],
 				[1, 2, 2, 2, 2, 2, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
@@ -92,7 +188,7 @@
      * Esto se llama antes de iniciar el juego
      */
     processing.setup = function () {
-      //framerate, no tocar puede malograr el codigo
+      //framerate, no tocar , solo para testear, pero volver a ponerlo en 60.
       processing.frameRate(60);
       processing.size(WIDTH, HEIGHT);
       //Carga las imagenes de los muros, monedas y Tomb
@@ -104,9 +200,10 @@
       MONEDA = processing.loadImage("images/moneda.png");
 
       //Crea los atributos internos de Tomb y de moneda(moneda actualmente no esta editado y sigue sin uso)
-      processing.state = {tomb: { x:11*SIZE, y:13*SIZE, width:SIZE, height:SIZE, dirx: 0, diry: 0}, moneda: [{ x: 1, y: 2 }, { x: 2, y: 1 }] };
+      processing.state = {tomb: { x:11*SIZE, y:13*SIZE, width:(SIZE/1.5), height:(SIZE/1.5), dirx: 0, diry: 0}, moneda: [{ x: 1, y: 2 }, { x: 2, y: 1 }] };
       
     }
+
     // Dibuja algo en el canvas. Aqui se pone todo lo que quieras pintar
     processing.drawGame = function (world) {
       processing.background(0, 0, 0);
@@ -115,37 +212,41 @@
       // Dibuja el mapa en filas, dando una imagen a cada celda
       recursivelist(mapa, (row, i) => {
         recursivelist(row, (cell, j) => {
-          if(cell == 1) { //muro centrado
+          if(cell == 1) { // Imagen de muro centrado
             murito1=processing.image(MURO1, j * SIZE, i * SIZE, SIZE, SIZE);
-            //hacer un rectangulo que guarde la imagen de muro y tenga sus propios atributos para luego hacer colisiones
-            // para no tener que hacer muchos if solo pon mas condicionales en el if con || o &&
           }
           if (cell == 2) {
-            //le da imagen a las monedas
+            //Imagen de las monedas
             processing.image(MONEDA, j * SIZE + SIZE / 3, i * SIZE + SIZE / 3, SIZE / 2.5, SIZE /2.5 );
           }
-          if(cell == 3) { //muro ladeado derecha
+          if(cell == 3) { //Imagen de muro ladeado derecha
             processing.image(MURO2, j * SIZE, i * SIZE, SIZE, SIZE);
           }
-          if(cell == 4) { //muro ladeado izquierda
+          if(cell == 4) { //Imagen de muro ladeado izquierda
             processing.image(MURO3, j * SIZE, i * SIZE, SIZE, SIZE);
           }
-          if(cell == 5) { //muro dañado
+          if(cell == 5) { //Imagen de muro dañado
             processing.image(MURO4, j * SIZE, i * SIZE, SIZE, SIZE);
           }
-          if(cell == 6) { //futuro muro de fuego
+          if(cell == 6) { //Imagen de futuro muro de fuego
             processing.image(MURO4, j * SIZE, i * SIZE, SIZE, SIZE);
           }
         });
       });
+
+      //codigos de testeo para verificar posicion de tomb en todo momento
       //voy a comentar esto un momento:
-      // console.log(world.tomb);
+      //console.log(world.tomb);
       //console.log(posicionActualTomb);
       
-      //Actualiza al mundo Tomb dibujandolo a lo largo de su camino
+
+
+      //Solo actualiza al mundo Tomb dibujandolo a lo largo de su camino.
       if (world.time == 0)
+        //imageMode(CENTER);
         processing.image(tomb, world.tomb.x , world.tomb.y, world.tomb.width,world.tomb.height);
       else
+        //imageMode(CENTER);
         processing.image(tomb, world.tomb.x , world.tomb.y, world.tomb.width,world.tomb.height);
       
     }
@@ -153,51 +254,142 @@
    
     processing.onTic = function (world) {
       
-      // Actualiza el tiempo lo que hace que tomb tenga su posicion en x mas la velocidad dada desde el teclado
+      // Actualiza el tiempo lo que hace que tomb tenga su posicion en X y Y mas la velocidad respectiva entregada desde el teclado (onKeyEvent).
       
-      if(world.tomb.x + world.tomb.width >= WIDTH+4)
-      {
-        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x - world.tomb.dirx, y: world.tomb.y - world.tomb.diry, width:SIZE, height:SIZE, dirx: 0, diry: world.tomb.diry}});
-      }
-      else if(world.tomb.x <= -4)
-      {
-        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x - world.tomb.dirx, y: world.tomb.y - world.tomb.diry, width:SIZE, height:SIZE, dirx: 0, diry: world.tomb.diry}});
-      }
-      else if(world.tomb.y+ world.tomb.height >= HEIGHT+4)
-      {
-        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y - world.tomb.diry, width:SIZE, height:SIZE, dirx: world.tomb.dirx, diry: 0}});
-      }
-      else if(world.tomb.y <= -4)
-      {
-        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y - world.tomb.diry, width:SIZE, height:SIZE, dirx: world.tomb.dirx, diry: 0}});
-      }
-      else
-      {
-        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y + world.tomb.diry, width:SIZE, height:SIZE, dirx: world.tomb.dirx, diry: world.tomb.diry}});
-      }
- 
     
+      //Decir lo que hay en la posicion de tomb
+      px=(posicionActualTomb.x)
+      py=(posicionActualTomb.y)
 
-      return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y + world.tomb.diry, width:SIZE, height:SIZE, dirx: world.tomb.dirx, diry: world.tomb.diry}});
+      if(world.tomb.x<=WIDTH && world.tomb.x>=1){
+        //console.log (world.tomb.y);        
+        a=posicion(mapa,py);
+        //console.log (posicion(a,px));
+
+        if(posicion(a,px) == 2){
+        //  console.log("aquí hay una moneda");
+          mapa=(replaceX(py,mapa,comeMoneda(px,posicion(mapa,py)))); //actualiza el mundo
+
+        }else{
+         // console.log("no hay moneda");//por lo tanto no hace nada
+        }
+      }
+
+      ////////////
+      
+
+      // Todas las condiciones siguientes usan la funcion recorrerBloques() y dentro de esta funcion  se usa las siguientes funciones: colisionTomb(), posicion(). los argumentos que usa la funcion recorrerBloques() es: listaBloques creada en la linea 83, la estructura creada apartir de la funcion calcularPosicionTomb(); en el otro lado de la condicion de recorrerBloques() es... .
+
+      //Colision a lo largo del tiempo en el eje Y,
+      //Cuando pasa la condicion adecuada pone la velocidad en 0, pone el booleano quietoY en true, lo que significa que tomb esta quieto en el momento.
+     
+      if( recorrerBloques(listaBloques, px, py-1) && (world.tomb.diry ==-4))
+      {
+        quietoY=true;
+        console.log("Choco contra la pared de arriba y paro")
+        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x , y: world.tomb.y + world.tomb.diry, width: world.tomb.width, height:world.tomb.height, dirx: world.tomb.dirx, diry: 0}});
+      }
+      else if( recorrerBloques(listaBloques, px, py+1) && (world.tomb.diry ==4))
+      {
+        console.log("Choco contra la pared de abajo y paro");
+        quietoY=true;
+        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x , y: world.tomb.y + world.tomb.diry, width: world.tomb.width, height:world.tomb.height, dirx: world.tomb.dirx, diry: 0}});
+      }
+
+      //Colision a lo largo del tiempo en el eje X
+      //Cuando pasa la condicion adecuada pone la velocidad en 0, pone el booleano quietoX en true, lo que significa que tomb esta quieto en el momento.
+      
+      if( recorrerBloques(listaBloques, px-1, py) && (world.tomb.dirx ==-4))
+      {
+        console.log("Choco contra la pared izq y paro")
+        quietoX=true;
+        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y + world.tomb.diry, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: world.tomb.diry}});
+      }
+      else if( recorrerBloques(listaBloques,px+1, py) && (world.tomb.dirx ==4))
+      {
+        console.log("Choco contra la pared derecha y paro")
+        quietoX=true;
+        return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y + world.tomb.diry, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: world.tomb.diry}});
+      }
+      
+      //Esta linea 213, hace que tomb siga actualizandose cuando no haya ningun bloque al lado de el como las anteriores condicionales.
+
+      return make(world, { time: world.time + 1, tomb: { x: world.tomb.x + world.tomb.dirx, y: world.tomb.y + world.tomb.diry, width:world.tomb.width, height:world.tomb.height, dirx: world.tomb.dirx, diry: world.tomb.diry}});
+ 
     }
 
   
-    //Cada vez que se presione una direccion va a tener una velocidad en X o Y, segun el caso.
+
+
+
+
+
+
+  
+    //Cada vez que se presione una direccion va a tener una velocidad en X, Y ó no tener velocidad segun las condicionales.
 
     processing.onKeyEvent = function (world, keyCode) {
-     //   console.log(world.tomb);
+    
+      // Todas las condiciones siguientes usan la funcion recorrerBloques() y dentro de esta funcion  se usa las siguientes funciones: colisionTomb(), posicion(). los argumentos que usa la funcion recorrerBloques() es: listaBloques creada en la linea 83, la estructura creada apartir de la funcion calcularPosicionTomb(); en el otro lado de la condicion de recorrerBloques() son dos booleanos creados para detectar si tomb esta en movimiento o no en los ejes Y y X.
+
+      // Movimiento hacia arriba de Tomb, tiene la condicion que si esta quieto y arriba de el hay un bloque, no puede moverse mas, y no es asi cambiara su velocidad y pondre la condicion en false.
+
       if (keyCode==processing.UP)
       {
-        return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width:SIZE, height:SIZE, dirx: 0, diry: -4 }} );
+        if( recorrerBloques(listaBloques, px, py-1) && quietoY== true )
+        {
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: 0 }} );
+        }
+        else
+        { 
+          quietoY=false;
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: -4 }} );
+        }
       }
-      if (keyCode==processing.DOWN){
-        return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width:SIZE, height:SIZE, dirx: 0, diry: 4 }} );
+
+      // Movimiento hacia abajo de Tomb, tiene la condicion que si esta quieto y abajo hay un bloque, no puede moverse mas, y no es asi cambiara su velocidad y pondra la condicion en false.
+
+      if (keyCode==processing.DOWN)
+      {
+        if( recorrerBloques(listaBloques, px, py+1) && quietoY== true )
+        {
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: 0 }} );
+        }
+        else
+        {
+          quietoY=false;
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: 4 }} );
+        }
       }
-      if (keyCode==processing.LEFT){
-        return make(world, {tomb: { x: world.tomb.x , y: world.tomb.y, width:SIZE, height:SIZE, dirx: -4, diry: 0 }} );
+
+      // Movimiento hacia la izquierda de Tomb, tiene la condicion que si esta quieto y a su izquierda hay un bloque, no puede moverse mas, y no es asi cambiara su velocidad y pondra la condicion en false.
+
+      if (keyCode==processing.LEFT)
+      {
+        if( recorrerBloques(listaBloques,px-1,py) && quietoX==true )
+        {
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: 0 }} );
+        }
+        else
+        {
+          quietoX=false;
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: -4, diry: 0 }} );
+        }
       }
-      if (keyCode==processing.RIGHT){
-        return make(world, {tomb: { x: world.tomb.x ,y: world.tomb.y, width:SIZE, height:SIZE, dirx: 4, diry: 0 }} );
+
+      // Movimiento hacia la derecha de Tomb, tiene la condicion que si esta quieto y a su derecha hay un bloque, no puede moverse mas, y no es asi cambiara su velocidad y pondra la condicion en false.
+
+      if (keyCode==processing.RIGHT)
+      {
+        if( recorrerBloques(listaBloques,px+1,py) && quietoX==true )
+        {
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 0, diry: 0 }} );
+        }
+        else
+        {
+          quietoX=false;
+          return make(world,{tomb:{ x: world.tomb.x, y: world.tomb.y, width: world.tomb.width, height:world.tomb.height, dirx: 4, diry: 0 }} );
+        }
       }
     }
 
